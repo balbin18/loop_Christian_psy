83002768df1cae3ad67609c60bff448f124d14a5
balbin18 <45374964+balbin18@users.noreply.github.com>
2018-11-27 07:57:47 +0800

    Create Dynamic_input_Balbinnnnnn.py

---
+++ Dynamic_input_Balbinnnnnn.py
@@ -0,0 +1,13 @@
+# Accept input from user
+
+name = input ("Enter your Name:")
+print("You're name is:"+ name)
+
+# Accept numerical input
+fnum = int(input("Enter first number:"))
+snum = int(input("Enter second number:"))
+
+to_int_value = int("20")
+
+print(fnum + snum)
+print(to_int_value ** 2)
