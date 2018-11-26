2e42985e152ef1b0f04aac13019d97f7d3b549d2
balbin18 <45374964+balbin18@users.noreply.github.com>
2018-11-27 07:56:04 +0800

    Create Sample_Balbin.py

---
+++ Sample_Balbin.py
@@ -0,0 +1,59 @@
+# Strings
+
+top = "haha"
+mid = "Huhu"
+bot = "hihi"
+support = "hehe"
+
+
+# Numerical Values
+
+number_of_creepings = 10
+number_of_kills = 11
+number_of_deaths = 12
+number_of_assists = 13
+
+
+print(top)
+print(mid)
+print(bot)
+print(support)
+
+
+
+print(number_of_assists)
+print(number_of_creepings)
+print(number_of_deaths)
+print(number_of_kills)
+
+
+# Arithmetic
+addition = 1 + 1
+subraction = 1 - 1
+multiplication = 1 * 1
+division = 1 / 1
+modolus = 5 % 3
+
+print(addition)
+print(subraction)
+print(multiplication)
+print(division)
+print(modolus)
+
+
+if addition < division:
+    print("Hello")
+    print("My")
+    print("Name")
+    print("Is")
+    print("Christian")
+    print("Lance")
+    print("B.")
+    print("Balbin")
+
+elif modolus < multiplication:
+    print("Christian")
+    print("Is")
+    print("Humble")
+else:
+    print("Wafuuuuuuu")
