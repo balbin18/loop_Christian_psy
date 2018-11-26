cd1c978af07d8d571d78c9a9a558c2b07ef7f779
balbin18 <45374964+balbin18@users.noreply.github.com>
2018-11-27 07:53:30 +0800

    Create loop_Balbin.py

---
+++ loop_Balbin.py
@@ -0,0 +1,20 @@
+print("\tBy 2 ")
+for z in range(2,21,2):
+    print(z)
+
+print("\tBy 4")
+for x in range(4,41,4):
+    print(x)
+print("\tBy 8")
+for c in range(8,81,8):
+    print(c)
+print("\tBy 12")
+for v in range(12,145,12):
+    print(v)
+# nested loop sample
+print("Multiplication table by 10 to 20")
+for a in range(10,21):
+    for b in range(1,11):
+
+        print( a * b, end ='\t')
+    print()
