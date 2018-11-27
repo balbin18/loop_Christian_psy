ed5ba5b72f070427b965fc246c3329b267052b3f
balbin18 <45374964+balbin18@users.noreply.github.com>
2018-11-27 09:53:11 +0800

    Create len.py

---
+++ len.py
@@ -0,0 +1,15 @@
+mga_dog = ["puti","red","itim"]
+mga_numero = [1,2,3,4,5,6]
+mga_decimal_points = [1.0,1.2,1.3,1.4]
+
+for x in range (len(mga_dog)):
+    print(mga_dog[x])
+for y in range (len(mga_numero)):
+    print(mga_numero[y])
+
+for z in range (len(mga_decimal_points)):
+    print(mga_decimal_points[z])
+
+print(len(mga_dog))
+print(len(mga_numero))
+print(len(mga_decimal_points))
