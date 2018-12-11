959f4a60249a1e0eb63f463eb0d91adf612d2397
balbin18 <45374964+balbin18@users.noreply.github.com>
2018-12-11 09:32:43 +0800

    Create Activity_A_Balbin.py

---
+++ Activity_A_Balbin.py
@@ -0,0 +1,25 @@
+# variables
+christian = 10
+lance = 10
+print(christian)
+print(lance)
+
+# operators
+balbs = 10
+balb = 10
+print(balbs + balb)
+
+# conditionals
+
+if (lance <= 10 and lance >= 0):
+    print("Congrats hehe")
+elif (lance <= 20 and lance >= 11):
+    print("Kamote kapa din")
+
+else:
+    print("Oraaayt")
+
+# Loops
+
+for x in range(1, 5, 1):
+    print("x")
