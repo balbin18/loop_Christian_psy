15b4a15ae4f36698bfdaff28cfdcef3728f28822
balbin18 <45374964+balbin18@users.noreply.github.com>
2019-01-22 11:01:42 +0800

    Create Splice_Balbinnnnn.py

---
+++ Splice_Balbinnnnn.py
@@ -0,0 +1,22 @@
+def LIST(x):
+    listX = x
+
+    print(listX[2:7])
+listX = [1, 2, 3, 4, 5, 6, 7]
+LIST(listX)
+
+def LIST (y):
+    listY = y
+
+    print(listY[1:3])
+
+listY = [2.5,4.5,6.5,]
+LIST(listY)
+
+def LIST(a):
+    listA = a
+
+    print(listA[0:3])
+
+listA = ["Christian","Lance","Balbin","BSIT","1-A"]
+LIST(listA)
