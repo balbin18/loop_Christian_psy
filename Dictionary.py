b182cafd4c13a43957256da9bf17ab4652cef2f0
balbin18 <45374964+balbin18@users.noreply.github.com>
2019-02-12 10:51:29 +0800

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -23,3 +23,23 @@ while True:
         print()
     else:
         print('\nInvalid choice')
+
+     
+        print("1 - Add Number")
+        print("2 - Search Number")
+
+dict_phone = {}
+while True:
+    choices = (input ("Enter Choices: "))
+
+    if choices == "1":
+        name = input("Enter name: ")
+        p_num = input("Enter #: ")
+        dict_phone[name] = p_num
+
+    elif choices == "2":
+        dict_phone[name] = p_num
+        Search = input("Enter name: ")
+        print(dict_phone[Search])
+    else:
+        break
